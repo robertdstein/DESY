2ae9d30cce006db7c3fb555305ba5dbccb142d74
robertdstein <robert.david.stein@hotmail.com>
2016-02-16 13:09:43 +0100

    Backup 16/02/2016

--- positioning/minimise.py
+++ positioning/minimise.py
@@ -2,7 +2,7 @@ import array, math
 
 import time
 import numpy as np
-#~ from iminuit import Minuit
+from iminuit import Minuit
 import lightdensity as ld
 import calculatearea as ca
 import cherenkovradius as cr
