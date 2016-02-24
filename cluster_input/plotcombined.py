7ef70b04dc44c31d9383c79eefa810a8dc1145d7
robertdstein <robert.david.stein@hotmail.com>
2016-02-24 16:42:57 +0100

    Backup 24/02/16

---
+++ cluster_input/plotcombined.py
@@ -0,0 +1,39 @@
+afspath = '/afs/desy.de/user/s/steinrob/Documents/DESY/positioning/'
+
+sys.path.append(afspath)
+
+import argparse, math, random, time
+import csv
+import numpy as np
+
+nparrallel = 10
+
+targetfolder = "reconstructeddata/"
+
+batchname= "executereconstructed"
+
+import plotz as pz
+import plotposition as pp
+import plotepn as pe
+import plotangle as pa
+import plotheight as ph
+import plotlikelihood as pl
+import optimisez as oz
+
+import initialise as i
+numberofhours, mincount, reconstructiongridwidth, orientation, eff, flux, area, solidangle, selectionefficiency = i.run()
+
+with open("orientations/"+ orientation +".csv", 'rb') as csvfile:
+	reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+	rowcount = 0
+	for row in reader:
+		rowcount +=1
+		
+reconstructdata = afspath + targetfolder + batchname + "_combined.csv"
+
+llcuts = oz.run(reconstructdata, rowcount, int(cfg.mincount), graph=False)
+pl.run(reconstructdata, graph=False, llcuts)
+pz.run(reconstructdata, rowcount, int(mincount), graph=False, llcuts)
+pp.run(reconstructdata, rowcount, int(mincount), graph=False, llcuts)
+pe.run(reconstructdata, rowcount, int(mincount), graph=False, llcuts)
+ph.run(reconstructdata, rowcount, int(mincount), graph=False, llcuts)
